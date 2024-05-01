eca2f75654b9315cb9acff26daf860bca756319b
Emerson Frasure <emersonfrasure@gmail.com>
2024-05-01 16:12:47 -0400

    add: minor tweaks

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -6,7 +6,7 @@ export default function body() {
             <h3 className ="title">About</h3>
             <p className = "text">I am a third year Computer Science and Engineering student at The Ohio State University. I interned at Grange Insurance as an Application Developer Intern in summer 2024. I am also apart of Code 4 Community at college where I develop website games that teach coding fundamentals. We then go and showcase these games to classrooms of local school children K-12 to help build their fundamentals in programming. </p>
             <h3 className ="title">Interests</h3>
-            <p className = "text">I am exetremely passionate about programming and am constantly working on new projects or learning new technologys. In my free time I also love to rock climb, play tennis or any sport, watch football and longboard! I love being outdoors and visiting new places and am always done to try new activites.</p>
+            <p className = "text">I am exetremely passionate about programming and am constantly working on new projects or learning new technologies. In my free time I also love to rock climb, play any sport, watch football and longboard! I love being outdoors and visiting new places and am always done to try new activites.</p>
         </div>
     )
 }
