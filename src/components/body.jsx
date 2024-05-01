01b11770f22039f1d6bed9ffbcce35044a0521b1
Emerson Frasure <emersonfrasure@gmail.com>
2024-04-30 21:00:23 -0400

    add: Changed filler text and styling

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,9 +4,9 @@ export default function body() {
     return (
         <div id = "info">
             <h3 className ="title">About</h3>
-            <p className = "text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus in metus vulputate eu scelerisque felis imperdiet. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Proin libero nunc consequat interdum varius sit. Nunc lobortis mattis aliquam faucibus purus in. Tortor at risus viverra adipiscing. </p>
+            <p className = "text">I am a third year Computer Science and Engineering student at The Ohio State University. I interned at Grange Insurance as an Application Developer Intern in summer 2024. I am also apart of Code 4 Community at college where I develop website games that teach coding fundamentals. We then go and showcase these games to classrooms of local school children K-12 to help build their fundamentals in programming. </p>
             <h3 className ="title">Interests</h3>
-            <p className = "text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus in metus vulputate eu scelerisque felis imperdiet. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis.  </p>
+            <p className = "text">I am exetremely passionate about programming and am constantly working on new projects or learning new technologys. In my free time I also love to rock climb, play tennis or any sport, watch football and longboard! I love being outdoors and visiting new places and am always done to try new activites.</p>
         </div>
     )
 }
